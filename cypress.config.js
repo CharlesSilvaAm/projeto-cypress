61aedeb9607b12714b7431e3a2170c113404251c
Charles Silva <98916039+CharlesSilvaAm@users.noreply.github.com>
2025-02-14 02:11:01 -0400

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,10 @@
 const { defineConfig } = require("cypress");
-const allure = require("allure-cypress");
 
 module.exports = defineConfig({
   projectId: "2duc1j",
   e2e: {
     setupNodeEvents(on, config) {
-      allure(on, config, {
-        resultsDir: "allure-results",
-      });
+    
       return config;
     },
   },
